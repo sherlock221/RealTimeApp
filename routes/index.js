cba0ca946358560ad27db7e6b4bf5329ef00320c
sherlock <920825209@qq.com>
2014-11-05 00:08:13 +0800

    添加async模块

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,51 @@
 var express = require('express');
+var async = require("async");
 var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+
+    var insert = function (callback) {
+        setTimeout(function () {
+            console.log("insert");
+            callback(null, 'one');
+        }, 1000);
+
+    }
+    var select = function (callback) {
+        setTimeout(function () {
+            console.log("select");
+            callback(null, 'two');
+        }, 300);
+
+    }
+
+    var update = function (callback) {
+        console.log("update");
+        callback(null, 'three');
+    }
+
+    //串行执行
+    //行执行，一个函数数组中的每个函数，每一个函数执行完成之后才能执行下一个函数。
+    //遇到错误 则会打断 进入err函数
+//    async.series([insert,select,update],function(err,result){
+//        if(err){
+//            console.log(err);
+//        }
+//        console.log(result);
+//    });
+
+    //并行执行
+    //每个函数都是立即执行，不需要等待其它函数先执行。传给最终callback的数组中的数据按照tasks中声明的顺序，而不是执行完成的顺序。
+//    async.parallel([insert,select,update],function(err,result){
+//        if(err){
+//            console.log(err);
+//        }
+//        console.log(result);
+//    });
+
 });
-
-
 
 
 module.exports = router;
--- routes/socket.js
+++ routes/socket.js
@@ -1,22 +1,38 @@
+var  JString = require("../util/jstring");
+
+
+
 /**
  * websocket 处理
  * @param io
  */
 module.exports.use = function(io){
+
+    //当前连接客户端数量
+    var  clients = new JString.Map();
+
+    //监听 客户端连接
     io.on('connection', function(socket){
-        //推送信息
-        socket.emit('news', { hello: 'world' });
+        clients.put(socket.id,socket);
+        console.log(clients.size());
 
         //接受客户端的信息
-        socket.on('client', function (data) {
-            console.log(data);
+        socket.on('message', function (data) {
+            //点对点
+           this.emit("message_client",{test:this.id});
+            //广播所有的客户端
+           // io.sockets.emit("message_client",{test:this.id});
         });
 
         //客户端断开 socket被关闭
         socket.on('disconnect', function () {
-            console.log("disconnect...");
-            io.sockets.emit('user disconnected');
+            console.log("disconnect...",this.id);
+            clients.remove(this.id);
         });
+
     });
+
+
+
 
 }
